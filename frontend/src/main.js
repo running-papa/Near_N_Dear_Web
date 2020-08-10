a525f651721ddc878ebba94b06af3cb81e754db1
running-papa <qkrwls730@gmail.com>
2020-08-10 18:08:29 +0900

    master_init

---
+++ frontend/src/main.js
@@ -0,0 +1,17 @@
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import './plugins'
+import axios from 'axios'
+
+Vue.use(axios)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  vuetify,
+  render: h => h(App),
+}).$mount('#app')
